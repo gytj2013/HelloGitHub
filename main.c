3da7f280802bb435373a9ec70229c53bc96e2705
김효서 <gytj2013@naver.com>
2020-04-23 17:10:06 +0900

    과제 1

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-	printf("Hello World!! testbranch");
+	printf("Hello World!! testbranch1");
 	return 0;
 }
---
+++ testbranch3.py
@@ -0,0 +1,6 @@
+num = int(input("Enter the number : "))
+if num > 0 :
+    print("양수")
+else :
+    if num == 0 : print("영(zero)")
+    else : print("음수")
